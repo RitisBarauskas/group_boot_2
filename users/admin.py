dde72fac5c4e2175c1052fd3662e15b6223283ca
Ritis Barauskas <perm300ru@yandex.ru>
2023-12-11 16:48:21 +0300

    YAMDB-999: added roles for user

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserYaMDb
+
+
+@admin.register(UserYaMDb)
+class UserYaMDbAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'role')
+    search_fields = ('username',)
+    list_filter = ('role',)
+    empty_value_display = '-пусто-'
