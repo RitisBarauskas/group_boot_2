a1dd4d127c2b9959bcfe6e38d407e70e7d245200
Ritis Barauskas <perm300ru@yandex.ru>
2023-12-11 16:19:07 +0300

    YAMDB-0: changed AUTH_USER_MODEL

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class UserYaMDb(AbstractUser):
+    email = models.EmailField(verbose_name='Электронная почта', unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('first_name', 'last_name', 'username')
